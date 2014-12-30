1ddbe929ec24194bc32ee4fd9b4d07b954eedcd9
MPechorin <maksimp@wix.com>
2014-12-30 10:35:26 +0200

    fixing crash on json absence

--- src/blorg/model.clj
+++ src/blorg/model.clj
@@ -1,5 +1,6 @@
 (ns blorg.model
   (:require [cheshire.core :as json])
+  (:require [clojure.java.io :as io])
   (:import [java.util Timer TimerTask]))
 
 (def posts (atom nil))
@@ -10,8 +11,11 @@
 (defn save-posts []
   (spit "posts.json" (json/generate-string @posts)))
 
+; .exists is a method of Java's File
 (defn load-posts-file [file]
-  (json/parse-string (slurp file) true))
+  (if (.exists (io/file file)) ; Maksim added - was crashing on missing file
+    (json/parse-string (slurp file) true)
+    []))
 
 (defn load-posts []
   (reset! posts (load-posts-file "posts.json")))
